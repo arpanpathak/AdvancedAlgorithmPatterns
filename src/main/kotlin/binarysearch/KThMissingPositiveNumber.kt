bc1c823b27b8436b8116151a72bed488a1755ddc
Arpan Pathak <arpanpathak1996@gmail.com>
2024-12-30 18:18:17 -0800

    Add binary search problems

---
+++ src/main/kotlin/binarysearch/KThMissingPositiveNumber.kt
@@ -0,0 +1,21 @@
+package binarysearch
+
+import jdk.vm.ci.code.CodeUtil.K
+
+class KThMissingPositiveNumber {
+    fun findKthPositive(arr: IntArray, k: Int): Int {
+        var (left, right) = 0 to arr.size
+
+        while (left < right) {
+            val mid = left + (right - left) / 2
+            val missingCount = arr[mid] - ( mid + 1 )
+
+            when {
+                missingCount < k -> left = mid + 1
+                else -> right = mid
+            }
+        }
+
+        return left + k
+    }
+}
