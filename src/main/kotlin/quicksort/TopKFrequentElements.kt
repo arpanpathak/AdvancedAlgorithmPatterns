689584762dc0b05bbfedc1c366c81fdb5fb5d95d
Arpan Pathak <arpanpathak1996@gmail.com>
2025-01-01 22:06:54 -0800

    Yay I'm still worthy

---
+++ src/main/kotlin/quicksort/TopKFrequentElements.kt
@@ -0,0 +1,51 @@
+package quicksort
+
+import kotlin.random.Random
+
+class TopKFrequentElements {
+    private val map = HashMap<Int, Int>()
+
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        for (num in nums) {
+            map[num] = map.getOrDefault(num, 0) + 1
+        }
+
+        val uniqueNums = map.keys.toIntArray()
+        var start = 0
+        var end = uniqueNums.size - 1
+
+        while (start < end) {
+            val partitionIndex = partition(uniqueNums, start, end)
+            when {
+                partitionIndex < k - 1 -> start = partitionIndex + 1
+                partitionIndex > k - 1 -> end = partitionIndex - 1
+                else -> break
+            }
+        }
+
+        return uniqueNums.copyOfRange(0, k)
+    }
+
+    // Randomized Quick Partition...
+    private fun partition(nums: IntArray, start: Int, end: Int): Int {
+        val randomIndex = Random.nextInt(start, end + 1)
+        swap(nums, randomIndex, end)  // Swap pivot with the end
+        val pivot = map[nums[end]] ?: 0
+
+        var partitionIndex = start
+        for (i in start until end) {
+            if (map[nums[i]]!! >= pivot) {
+                swap(nums, i, partitionIndex++)
+            }
+        }
+
+        swap(nums, partitionIndex, end)  // Swap back the pivot to the correct position
+        return partitionIndex
+    }
+
+    private fun swap(nums: IntArray, i: Int, j: Int) {
+        val temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+    }
+}
