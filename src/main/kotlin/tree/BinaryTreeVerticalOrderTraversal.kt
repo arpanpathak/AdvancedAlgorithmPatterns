f42c6bb6b11bea1c15b93a7a21966f7aad75508d
Arpan Pathak <arpanpathak1996@gmail.com>
2025-01-01 18:54:28 -0800

    Refactored the ugly code of level order traversal

--- src/main/kotlin/tree/BinaryTreeVerticalOrderTraversal.kt
+++ src/main/kotlin/tree/BinaryTreeVerticalOrderTraversal.kt
@@ -34,26 +34,14 @@ class BinaryTreeVerticalOrderTraversal {
         queue.offer(VerticalIndex(root, 0))
 
         while (queue.isNotEmpty()) {
-            val size = queue.size
-            val levelNodes = mutableMapOf<Int, ArrayList<Int>>()
+            val (node, verticalIndex) = queue.poll()
 
-            for (i in 0 until size) {
-                val (node, verticalIndex) = queue.poll()
+            // Add node value directly to the result map
+            result.computeIfAbsent(verticalIndex) { ArrayList() }.add(node.`val`)
 
-                // Add node value to the corresponding vertical index in this level
-                val bucket = levelNodes.getOrPut(verticalIndex) { ArrayList() }
-                bucket.add(node.`val`)
-
-                // Add left and right children to the queue with updated vertical indices
-                node.left?.let { queue.offer(VerticalIndex(it, verticalIndex - 1)) }
-                node.right?.let { queue.offer(VerticalIndex(it, verticalIndex + 1)) }
-            }
-
-            // Merge the level nodes into the result map
-            for ((index, nodes) in levelNodes) {
-                val bucket = result.getOrPut(index) { ArrayList() }
-                bucket.addAll(nodes)
-            }
+            // Add left and right children to the queue with updated vertical indices
+            node.left?.let { queue.offer(VerticalIndex(it, verticalIndex - 1)) }
+            node.right?.let { queue.offer(VerticalIndex(it, verticalIndex + 1)) }
         }
 
         // Return the values from the TreeMap, which are already sorted by vertical index
