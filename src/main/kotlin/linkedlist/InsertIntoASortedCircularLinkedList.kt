ce126af2ef21474b9671a248c07ba8ee7279c8da
Arpan Pathak <arpanpathak1996@gmail.com>
2025-01-07 00:24:11 -0800

    Add circular linkedlist problems

---
+++ src/main/kotlin/linkedlist/InsertIntoASortedCircularLinkedList.kt
@@ -0,0 +1,29 @@
+package linkedlist
+
+class InsertIntoASortedCircularLinkedList {
+    fun insert(head: Node?, insertVal: Int): Node? {
+        val newNode = Node(insertVal)
+        if (head == null) return newNode.apply { next = newNode }
+
+        var current: Node? = head
+        do {
+            when {
+                current?.`val`!! <= insertVal && insertVal <= current?.next?.`val`!! -> {
+                    newNode.next = current?.next
+                    current?.next = newNode
+                    return head
+                }
+                current?.`val`!! > current?.next?.`val`!! && (insertVal >= current?.`val`!! || insertVal <= current?.next?.`val`!!) -> {
+                    newNode.next = current?.next
+                    current?.next = newNode
+                    return head
+                }
+            }
+            current = current?.next
+        } while (current != head)
+
+        newNode.next = current?.next
+        current?.next = newNode
+        return head
+    }
+}
