70a8ab0abf8ad237375335c0a9396777aad56ace
Arpan Pathak <arpanpathak1996@gmail.com>
2025-02-04 16:15:12 -0800

    Improve readibility of sliding window problem

--- src/main/kotlin/sliding_window/MaxConsecutiveOnes_III.kt
+++ src/main/kotlin/sliding_window/MaxConsecutiveOnes_III.kt
@@ -17,4 +17,25 @@ class MaxConsecutiveOnes_III {
 
         return right - left
     }
+
+    // Another intuitive solution using while loop
+    fun longestOnes1(nums: IntArray, k: Int): Int {
+        var left = 0
+        var remainingK = k
+        var maxLen = 0
+
+        for (right in nums.indices) {
+            if (nums[right] == 0) remainingK--
+
+            // If the window becomes invalid, move `left` forward
+            while (remainingK < 0) {
+                if (nums[left++] == 0) remainingK++
+            }
+
+            // Update the maximum window size
+            maxLen = maxOf(maxLen, right - left + 1)
+        }
+
+        return maxLen
+    }
 }
