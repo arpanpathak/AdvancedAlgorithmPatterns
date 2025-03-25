c25bda40677633bed5d39a84992484f8e319bbd2
Arpan Pathak <arpanpathak1996@gmail.com>
2025-03-24 20:47:03 -0700

    Add stack based greedy problems

---
+++ src/main/kotlin/stack/RemoveDuplicateLetters.kt
@@ -0,0 +1,27 @@
+package stack
+
+class RemoveDuplicateLetters {
+    fun removeDuplicateLetters(s: String): String {
+        val lastIndex = IntArray(26) {-1}
+        val inStack = BooleanArray(26)
+        val stack = ArrayDeque<Char>()
+
+        for (i in s.indices)
+            lastIndex[s[i] - 'a'] = i
+
+        for (i in s.indices) {
+            val c = s[i]
+
+            if (inStack[c - 'a']) continue
+
+            while (stack.isNotEmpty() && stack.last() > c && lastIndex[stack.last() - 'a'] > i)  {
+                inStack[stack.removeLast() - 'a'] = false
+            }
+
+            stack.addLast(c)
+            inStack[c - 'a'] = true
+        }
+
+        return stack.joinToString("")
+    }
+}
