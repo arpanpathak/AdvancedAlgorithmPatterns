74b2c47476513d4fe5253b2aa3591c145986db2a
Arpan Pathak <arpanpathak1996@gmail.com>
2026-01-01 16:11:50 -0800

    Improve readability of Frog jump using top down recursive caching

---
+++ src/main/kotlin/dynamic_programming/FrogJumpTopDown.kt
@@ -0,0 +1,20 @@
+package dynamic_programming
+
+class FrogJumpTopDown {
+    data class State(val pos: Int, val k: Int)
+
+    fun canCross(stones: IntArray): Boolean {
+        val stoneSet = stones.toSet()
+        val cache = mutableMapOf<State, Boolean>()
+
+        fun solve(pos: Int, k: Int): Boolean =
+            cache.getOrPut(State(pos, k)) {
+                pos == stones.last() || (k - 1..k + 1).any { nextJump ->
+                    nextJump > 0 &&
+                    (pos + nextJump) in stoneSet && solve(pos + nextJump, nextJump)
+                }
+            }
+
+        return stones.getOrNull(1) == 1 && solve(1, 1)
+    }
+}
