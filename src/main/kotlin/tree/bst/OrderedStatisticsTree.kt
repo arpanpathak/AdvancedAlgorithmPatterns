e288b9f06e318d60f922f11d1d2b1187e63f5f87
Arpan Pathak <arpanpathak1996@gmail.com>
2026-01-10 07:58:25 -0800

    Update order statistics tree with insert operation

--- src/main/kotlin/tree/bst/OrderedStatisticsTree.kt
+++ src/main/kotlin/tree/bst/OrderedStatisticsTree.kt
@@ -8,6 +8,18 @@ class OrderedStatisticsTree {
         var size: Int = 1 // New field
     }
 
+    fun insert(node: TreeNode?, value: Int): TreeNode {
+        when {
+            node == null -> return TreeNode(value)
+            value < node.`val` -> node.left = insert(node.left, value)
+            else -> node.right = insert(node.right, value)
+        }
+
+        // Update size after the child has been inserted
+        node.size = (node.left?.size ?: 0) + (node.right?.size ?: 0) + 1
+        return node
+    }
+
     class Solution {
         fun findKth(node: TreeNode?, k: Int): Int {
             if (node == null) return -1
