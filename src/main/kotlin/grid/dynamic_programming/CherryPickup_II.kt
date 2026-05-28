a9f456482cc230425da852dcea3aac2668a4ea0e
Arpan Pathak <arpanpathak1996@gmail.com>
2026-05-28 16:50:58 -0700

    Fix A* algorithm bug

--- src/main/kotlin/grid/dynamic_programming/CherryPickup_II.kt
+++ src/main/kotlin/grid/dynamic_programming/CherryPickup_II.kt
@@ -1,6 +1,7 @@
 package grid.dynamic_programming
 
 class CherryPickup_II {
+    private val NEGATIVE_INFINITY = -1e8.toInt()
     data class State(val r: Int, val c1: Int, val c2: Int)
 
     fun cherryPickup(grid: Array<IntArray>): Int {
@@ -9,14 +10,13 @@ class CherryPickup_II {
 
         // Column offsets for robots
         val dirs = intArrayOf(-1, 0, 1)
-
         fun pickupCherry(row: Int, robot1Col: Int, robot2Col: Int)
             = if (robot1Col == robot2Col) grid[row][robot1Col] else grid[row][robot1Col] + grid[row][robot2Col]
 
         fun solve(r: Int, c1: Int, c2: Int): Int =
             cache.getOrPut(State(r, c1, c2)) {
                 when {
-                    c1 !in 0 until cols || c2 !in 0 until cols -> Int.MIN_VALUE
+                    c1 !in 0 until cols || c2 !in 0 until cols -> NEGATIVE_INFINITY
                     r == rows - 1 -> pickupCherry(r, c1, c2)
                     else -> {
                         pickupCherry(r, c1, c2) + dirs.maxOf { dc1 ->
